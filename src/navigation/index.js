8cc1bf0edf5c4b8165e97e41e34c9dd71323c660
VAISHNAVY <vaishnavyvs09@gmail.com>
2022-05-07 16:39:21 +0530

    Front screen creation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,12 +3,11 @@ import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import SignInScreen from '../screens/SignInScreen';
-import SignUpScreen from '../screens/SignUpScreen';
-import ConfirmEmailScreen from '../screens/ConfirmEmailScreen';
-import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
-import NewPasswordScreen from '../screens/NewPasswordScreen';
-import HomeScreen from '../screens/HomeScreen';
+import SignInScreen from '../views/SignInScreen';
+import ConfirmEmailScreen from '../views/ConfirmEmailScreen';
+import ForgotPasswordScreen from '../views/ForgotPasswordScreen';
+import NewPasswordScreen from '../views/NewPasswordScreen';
+import HomeScreen from '../views/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,13 +15,13 @@ const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="SignIn" component={SignInScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
         <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
 
-        <Stack.Screen name="Home" component={HomeScreen} />
+       
       </Stack.Navigator>
     </NavigationContainer>
   );
