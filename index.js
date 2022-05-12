eec49d7baa41995e52c75e18f2e9e12f00859866
kamsayiny <kamshasris01@gmail.com>
2022-05-12 20:41:28 +0530

    add a comment

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /**
  * @format
  */
+//comment one done by Kamsha
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
