7a71057876e2261aa248100fd32362e46cd1b9cb
kamsayiny <kamshasris01@gmail.com>
2022-05-20 14:01:14 +0530

    Add a comment

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 //import CustomButton from '../../components/CustomButton';
+// comment one
 const Dashboard = () => {
 
   const navigation = useNavigation();
