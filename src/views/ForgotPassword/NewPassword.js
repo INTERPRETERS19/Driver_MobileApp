16d223d112b9c763b6e555454a9eb5fa78bb8da7
Gayathri-Sivakumar <79824505+Gayathri-Sivakumar@users.noreply.github.com>
2022-05-27 21:45:36 +0530

    Settings (#27)
    * settings basic design
    
    * conflicchanged
    
    * no change
    
    * terms completed
    
    Co-authored-by: VaishnavyVS <65849779+VaishnavyVS@users.noreply.github.com>
    Co-authored-by: kamsayiny <75096464+KamsayinySrikaran@users.noreply.github.com>

--- src/views/ForgotPassword/NewPassword.js
+++ src/views/ForgotPassword/NewPassword.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import React, {useState} from 'react'
 import {
-ImageBackground,
+  ImageBackground,
   View,
   Text,
   StyleSheet,
@@ -10,76 +11,80 @@ ImageBackground,
 import CustomInput from '../../components/CustomInput';
 import COLORS from '../../components/colors';
 import CustomButton from '../../components/CustomButton';
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 
 const NewPassword = () => {
   const [password, setPassword] = useState('');
-  const {height} = useWindowDimensions();
+  const { height } = useWindowDimensions();
   const navigation = useNavigation();
 
-const onChangePressed = () => {
+  const onChangePressed = () => {
     navigation.navigate('ResetSuccess');
   };
 
- return (
-<ImageBackground
-          source={require('../../../assets/img1.jpg')}
-          style={{ flex: 1,
+  return (
+    <ImageBackground
+      source={require('../../../assets/img1.jpg')}
+      style={{
+        flex: 1,
 
-            height: '100%',
-            }}
-        >
-<ScrollView showsVerticalScrollIndicator={false}>
+        height: '100%',
+      }}
+    >
+      <ScrollView showsVerticalScrollIndicator={false}>
 
-        <View style={{marginTop: 160}}>
+        <View style={{ marginTop: 160 }}>
 
-          <Text style={{ fontFamily: 'Poppins',fontSize: 29, fontWeight: 'bold', textAlign: "center",
-    color: COLORS.dark}}>
+          <Text style={{
+            fontFamily: 'Poppins', fontSize: 29, fontWeight: 'bold', textAlign: "center",
+            color: COLORS.dark
+          }}>
             New Password
           </Text>
 
-          <Text style={{fontFamily: 'Poppins',
-                        fontSize: 17,
-                        fontWeight: 'bold',
-                        textAlign: "left",
-                        color: '#656363',
-                        marginTop: 50,
-                        marginBottom: 20,
-                        marginLeft: 15,
-                        marginRight: 15,
-                        backgroundColor: '#D2F3C6',
-                        borderRadius: 9,
-                        paddingHorizontal: 20,
-                        paddingVertical: 17,}}>
+          <Text style={{
+            fontFamily: 'Poppins',
+            fontSize: 17,
+            fontWeight: 'bold',
+            textAlign: "left",
+            color: '#656363',
+            marginTop: 50,
+            marginBottom: 20,
+            marginLeft: 15,
+            marginRight: 15,
+            backgroundColor: '#D2F3C6',
+            borderRadius: 9,
+            paddingHorizontal: 20,
+            paddingVertical: 17,
+          }}>
             Please create a new password that you don't use on any other site
           </Text>
         </View>
 
-         <View style={styles.root}>
-        <CustomInput
-          placeholder="Create New Password"
-          value={password}
-          setValue={setPassword}
-          secureTextEntry
-        />
+        <View style={styles.root}>
+          <CustomInput
+            placeholder="Create New Password"
+            value={password}
+            setValue={setPassword}
+            secureTextEntry
+          />
 
-        <CustomInput
-          placeholder="Confirm your Password"
-          value={password}
-          setValue={setPassword}
-          secureTextEntry
-        />
+          <CustomInput
+            placeholder="Confirm your Password"
+            value={password}
+            setValue={setPassword}
+            secureTextEntry
+          />
         </View>
 
-         <View style={{marginTop: 20}}>
-         <CustomButton
-         text="Change"
-         onPress={onChangePressed}
-         />
-         </View>
-
-</ScrollView>
-    </ImageBackground>
+        <View style={{ marginTop: 20 }}>
+          <CustomButton
+            text="Change"
+            onPress={onChangePressed}
+          />
+        </View>
+      </ScrollView>
+    </ImageBackground >
   );
 };
 
@@ -90,15 +95,15 @@ const styles = StyleSheet.create({
   },
 
   container: {
-        flex: 1,
-        alignItems: "center",
-      //marginLeft:25,
-        justifyContent: "center",
-      },
+    flex: 1,
+    alignItems: "center",
+    //marginLeft:25,
+    justifyContent: "center",
+  },
 
-      label: {
-        margin: 8,
-      },
+  label: {
+    margin: 8,
+  },
 
 });
 
