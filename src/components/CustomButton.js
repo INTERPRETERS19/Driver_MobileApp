504abdd6610cffe203b44bc6c25b0f15a9bc2099
mathiniK <mathini1k@gmail.com>
2022-05-30 09:03:01 +0530

    OTP

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -34,9 +34,9 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     shadowColor: '#000',
     shadowOffset: {
-      width: 0,
-      height: 5,
-    },
+    width: 0,
+    height: 5,
+  },
     shadowOpacity: 0.36,
     shadowRadius: 6.68,
   },
--- src/views/OTP/OTP.js
+++ src/views/OTP/OTP.js
@@ -6,30 +6,28 @@ ImageBackground,
   StyleSheet,
   useWindowDimensions,
   ScrollView,
-
 } from 'react-native';
 import CustomInput from '../../components/CustomInput';
 import COLORS from '../../components/colors';
 import CustomButton from '../../components/CustomButton';
 import {useNavigation} from '@react-navigation/native';
-
+import OTPTextView from 'react-native-otp-textinput';
 
 const OTP = () => {
-
   const {height} = useWindowDimensions();
   const navigation = useNavigation();
 
 const onVerifyPressed = () => {
     navigation.navigate('NewPassword');
   };
+
 const onBacktoMyAccountPressed = () => {
     navigation.navigate('SignIn');
   };
 
 const onResendPressed = () => {
-    navigation.navigate('SignIn');
+    navigation.navigate('OTP');
   };
-
 
   return (
   <ImageBackground
@@ -67,29 +65,32 @@ const onResendPressed = () => {
                               Enter OTP we just sent to your mobile {'\n'} number: +94******567
                             </Text>
 
-
+            <View>
+                <OTPTextView
+                          handleTextChange={(e) => {}}
+                          containerStyle={styles.textInputContainer}
+                          textInputStyle={styles.roundedTextInput}
+                          inputCount={4}
+                          inputCellLength={1}
+                        />
+            </View>
 
                   <CustomButton
                            text="Verify"
                            onPress={onVerifyPressed}
                            />
-                                        <View style={{marginLeft:231, marginTop: -10}}>
+                                        <View style={{marginLeft:228, marginTop: -17}}>
                                             <CustomButton
-                                             text="Re send"
-                                             color="#578FD1"
+                                             text="Re-send"
                                              onPress={onResendPressed}
                                              type="SUB"
                                              />
                                         </View>
 
                 </View>
-
-
                  </View>
 
-         <View style={{marginTop: 30}}>
-
-
+         <View>
          <CustomButton
                   text="Back to my Account"
                   onPress={onBacktoMyAccountPressed}
@@ -116,8 +117,17 @@ const styles = StyleSheet.create({
 
       },
 
-      label: {
-        margin: 8,
+      textInputContainer: {
+          marginBottom: 20,
+          marginTop: 20
+        },
+
+      roundedTextInput: {
+         width: 70,
+         height: 70,
+         borderRadius: 11,
+         borderWidth: 5,
+         backgroundColor:'#F5F6F7',
       },
 });
 
