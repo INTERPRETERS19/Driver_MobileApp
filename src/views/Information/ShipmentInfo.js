432c295a9813e70cc55028c000a8942c3b24478c
chambavy <chambavy@gmail.com>
2022-06-22 17:23:16 +0530

    drop-down

--- src/views/Information/ShipmentInfo.js
+++ src/views/Information/ShipmentInfo.js
@@ -7,54 +7,48 @@ import Dropdown from './Dropdown';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import CheckBox from '@react-native-community/checkbox';
-import Client from "../../routes/client";
+import { Picker } from '@react-native-picker/picker';
 
 const ShipmentInfo = () => {
   const navigation = useNavigation();
   //const [COD] = useState('');
-  const [isSelected, setSelection] = useState(false);
-  const onDonePressed = () => {
-    navigation.navigate('OutForDelivery');
-  };
   const SHIPPER_ID = "62b0b975ea359dca05630008";
   const [shipmentInfo, setShipmentInfo] = useState(
-    {data:{
-      recipient_name:"",
-      mobile_phone_number:"",
-      r_district:"",
-      r_city: "",
-      COD: "",
-      current_status:""
-    }}
+    {
+      data: {
+        id: "",
+        recipient_name: "",
+        mobile_phone_number: "",
+        r_district: "",
+        r_city: "",
+        COD: "",
+        current_status: ""
+      }
+    }
   );
   useEffect(() => {
     getUser(SHIPPER_ID);
   }, []);
 
-  // const getUser = async (userId) => {
-  //   await Client.get("/shipmentinfo", { _id: SHIPPER_ID })
-  //     .then((response) => {
-  //       setShipmentInfo(response);
-  //       console.log(shipmentInfo);
-  //     })
-  //     .catch((err) => {
-  //       console.log("Unable to get shipment info");
-  //     });
-  // };
   const getUser = async userId => {
-    await Client.get('/shipmentinfo', {_id: SHIPPER_ID})
+    await Client.get('/shipmentinfo', { _id: SHIPPER_ID })
       .then(response => {
         setShipmentInfo(response);
-        console.log(response);
+        console.log(response.data);
       })
       .catch(err => {
         console.log('Unable to get information');
       });
   };
-
+  const [isSelected, setSelection] = useState(false);
+  
+  const onDonePressed = () => {
+    navigation.navigate('OutForDelivery');
+  };
   const onbackPressed = () => {
     navigation.navigate('OutForDelivery');
   };
+  const [selectedValue, setSelectedValue] = useState('Out for delivery');
 
   return (
     <View style={styles.body}>
@@ -86,7 +80,7 @@ const ShipmentInfo = () => {
                 <Text style={styles.head}>Shipment ID</Text>
               </View>
               <View style={{ flex: 1 }}>
-                <Text style={styles.head}>XXXXXX</Text>
+                <Text style={styles.head}>{shipmentInfo.data.id}</Text>
               </View>
             </View>
             <View style={styles.info}>
@@ -104,25 +98,49 @@ const ShipmentInfo = () => {
                 <View style={{ flex: 3 }}>
                   <Text style={styles.form}>{shipmentInfo.data.COD}</Text>
                 </View>
-                <View style={{ flex: 1 }}>
-                  <CheckBox
-                    value={isSelected}
-                    onValueChange={setSelection}
-                    style={styles.checkbox}
-                  />
+
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                  <View style={{ flex: 3 }}>
+                    <Text style={styles.form}>1000</Text>
+                  </View>
+                  <View style={{ flex: 1 }}>
+                    <CheckBox
+                      value={isSelected}
+                      onValueChange={setSelection}
+                      style={styles.checkbox}
+                    />
+                  </View>
+                </View>
+
+                <Text style={styles.infoIn}>Status</Text>
+
+                {/* <Dropdown></Dropdown> */}
+                <View style={styles.container}>
+                  <Picker
+                    selectedValue={selectedValue}
+                    style={{
+                      display: 'flex',
+                      height: 50,
+                      width: 200,
+                      // marginTop: 500,
+                      alignSelf: 'center',
+                    }}
+                    onValueChange={(itemValue, itemIndex) =>
+                      setSelectedValue(itemValue)
+                    }>
+                    <Picker.Item label="Rescheduled" value="Rescheduled" />
+                    <Picker.Item label="Failed to Deliver" value="Failed to Deliver" />
+                    <Picker.Item label="Delivered" value="Delivered" />
+                    <Picker.Item label="Out for delivery" value="Out for delivery" />
+                  </Picker>
                 </View>
               </View>
-
-              <Text style={styles.infoIn}>Status</Text>
-              <Text style={styles.form}>{shipmentInfo.data.current_status}</Text>
-
-              {/* <Dropdown></Dropdown> */}
+            </View>
+            <View style={styles.button}>
+              <CustomButton text="Done" onPress={onDonePressed} />
             </View>
           </View>
-          <View style={styles.button}>
-            <CustomButton text="Done" onPress={onDonePressed} />
           </View>
-        </View>
       </ImageBackground>
     </View>
   );
