d50da5ead35af302fe62e51eff6e609e5571e5a8
kamsayiny <kamshasris01@gmail.com>
2022-07-03 20:52:06 +0530

    except css work

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -7,7 +7,6 @@ import {useLogin} from '../context/LoginProvider';
 import SignInScreen from '../views/SignInScreen/SignInScreen';
 import Dashboard from '../views/Dashboard/Dashboard';
 import OutForDelivery from '../views/Shipments/OutForDelivery';
-import PickUp from '../views/Shipments/PickUpShipment';
 import DeliveredShipment from '../views/Shipments/DeliveredShipment';
 import Summary from '../views/Shipments/Summary';
 import RescheduledShipment from '../views/Shipments/RescheduledShipment';
@@ -26,6 +25,7 @@ import PickUpInfo from '../views/Information/PickUpInfo';
 import ShipmentDetails from '../views/Shipments/ShipmentDetails';
 import QRScan from '../views/ScanPage/QRScan';
 import DeliveryFee from '../views/DeliveryFee/DeliveryFee';
+import PickUp from '../views/Shipments/PickUp';
 
 const Drawer = createDrawerNavigator();
 
@@ -135,7 +135,7 @@ function CustomDrawerContent(props) {
         </View>
         <View style={[styles.menuCard]}>
           <DrawerItem
-            label="Fail To Deliver"
+            label="Returns"
             labelStyle={[styles.NavPages]}
             onPress={() => {
               props.navigation.navigate('Returns');
@@ -174,7 +174,6 @@ const DrawerNavigator = () => {
         drawerContent={props => <CustomDrawerContent {...props} />}>
         <Drawer.Screen name="Dashboard" component={Dashboard} />
         <Drawer.Screen name="OutForDelivery" component={OutForDelivery} />
-        <Drawer.Screen name="PickUp" component={PickUp} />
         <Drawer.Screen name="DeliveredShipment" component={DeliveredShipment} />
         <Drawer.Screen name="Summary" component={Summary} />
         <Drawer.Screen
@@ -184,6 +183,8 @@ const DrawerNavigator = () => {
         <Drawer.Screen name="Returns" component={Returns} />
         <Drawer.Screen name="Collections" component={Collections} />
         <Drawer.Screen name="DeliveryFee" component={DeliveryFee} />
+        <Drawer.Screen name="PickUp" component={PickUp} />
+        <Drawer.Screen name="PickUpInfo" component={PickUpInfo} />
         <Drawer.Screen name="Settings" component={Settings} />
         <Drawer.Screen name="QRScan" component={QRScan} />
         <Drawer.Screen name="Profile" component={Profile} />
@@ -193,7 +194,6 @@ const DrawerNavigator = () => {
         <Drawer.Screen name="Terms" component={Terms} />
         <Drawer.Screen name="PrivacyPolicy" component={PrivacyPolicy} />
         <Drawer.Screen name="ShipmentInfo" component={ShipmentInfo} />
-        <Drawer.Screen name="PickUpInfo" component={PickUpInfo} />
         <Drawer.Screen name="ShipmentDetails" component={ShipmentDetails} />
       </Drawer.Navigator>
     </NavigationContainer>
